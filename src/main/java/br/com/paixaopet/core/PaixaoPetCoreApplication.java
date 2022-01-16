86019edb8dbd6f1471c4e3245d3924fc36b87122
Everton Luis Santos Pereira <everton.pereira@unico.io>
2022-01-15 23:28:12 -0300

    project generated with spring initializr

---
+++ src/main/java/br/com/paixaopet/core/PaixaoPetCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.paixaopet.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaixaoPetCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaixaoPetCoreApplication.class, args);
+	}
+
+}
