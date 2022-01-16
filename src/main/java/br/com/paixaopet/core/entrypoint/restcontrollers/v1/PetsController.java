b8428914f6574de9f4a15b6a3ad895c109cad041
Everton Luis Santos Pereira <everton.pereira@unico.io>
2022-01-16 00:29:01 -0300

    PetsController: created with fake resource POST /

---
+++ src/main/java/br/com/paixaopet/core/entrypoint/restcontrollers/v1/PetsController.java
@@ -0,0 +1,26 @@
+package br.com.paixaopet.core.entrypoint.restcontrollers.v1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static java.lang.String.format;
+import static org.springframework.http.ResponseEntity.created;
+
+@RestController
+@RequestMapping(PetsController.PATH)
+public class PetsController {
+
+    public static final String PATH = "/v1/pets";
+
+    @PostMapping
+    public ResponseEntity<Void> create() {
+        var id = "15";
+
+        var location = URI.create(PATH.concat(format("/%s", id)));
+        return created(location).build();
+    }
+}
