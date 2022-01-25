d7bf53d07848efd2e90331f0d1335e73b7c5d0d9
Everton Luis Santos Pereira <everton.pereira@unico.io>
2022-01-25 16:36:00 -0300

    ApplicationConfiguration: added enabling auto configuration

---
+++ src/main/java/br/com/paixaopet/core/configurations/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.paixaopet.core.configurations;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+public class ApplicationConfiguration {
+}
